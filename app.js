0c288f0fb21dbff94d0b2c617c7994ad950b9b3f
Thế Đạt Nguyễn <dnguyen@datoday.local>
2023-11-28 02:46:53 +0700

    test

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function (err, req, res, next) {
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	// render the error page
-	res.status(err.status || 500).send({ error: err.message })
+	res.status(err.status || 500).send({ err })
 	// res.render('error');
 });
 
