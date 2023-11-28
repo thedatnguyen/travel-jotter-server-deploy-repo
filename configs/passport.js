793ba1596aa2832de02b5137904670da56d6c0d8
Thế Đạt Nguyễn <dnguyen@datoday.local>
2023-11-28 13:21:55 +0700

    test

--- configs/passport.js
+++ configs/passport.js
@@ -7,9 +7,44 @@ const weavy = require('./weavy');
 const dropbox = require('./dropbox');
 const prisma = new PrismaClient();
 
-passport.serializeUser(function (user, cb) {
+passport.serializeUser(async function (profile, done) {
     try {
-        cb(null, user);
+        // console.log(user);
+        const googleAccount = profile._json;
+
+        const account = await prisma.account.findUnique({
+            where: { email: googleAccount.email }
+        })
+        console.log(account);
+
+        // account not registered yet: first login -> create new account
+        if (!account) {
+            console.log('create new account')
+            const googlePicture = (await axios.get(googleAccount.picture, { responseType: 'arraybuffer' })).data;
+            const pictureBuffer = Buffer.from(googlePicture, 'base64');
+            const { pictureUrl, pictureId } = await dropbox.uploadImage(pictureBuffer);
+            const newAccount = {
+                email: googleAccount.email,
+                username: googleAccount.email,
+                gender: 'not_defined',
+                firstName: googleAccount.given_name,
+                lastName: googleAccount.family_name,
+                hashedPassword: '',
+                pictureUrl,
+                pictureId,
+                phoneNumber: '',
+            }
+
+            newAccount.chatAccountId = await weavy.createUser(newAccount)
+
+            const newAccountData = await prisma.account.create({
+                data: newAccount
+            })
+
+            console.log(newAccountData)
+        }
+
+        done(null, profile);
         console.log(`serialize success`);
     } catch (error) {
         console.log(`serialize failed`);
@@ -17,16 +52,17 @@ passport.serializeUser(function (user, cb) {
     }
 });
 
-passport.deserializeUser(function (obj, cb) {
-    try {
-        cb(null, obj);
-        console.log(`deserialize success`);
-    } catch (error) {
-        console.log(`deserialize failed`);
-        console.log(error);
-    }
-    
-});
+// passport.deserializeUser(async function (profile, done) {
+//     try {
+//         console.log(profile);
+//         done(null, profile);
+//         console.log(`deserialize success`);
+//     } catch (error) {
+//         console.log(`deserialize failed`);
+//         console.log(error);
+//     }
+
+// });
 
 passport.use(new GoogleStrategy(
     {
@@ -34,47 +70,47 @@ passport.use(new GoogleStrategy(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/callback'
     },
-    async (accessToken, refreshToken, profile, done) => {
-        try {
-            const googleAccount = profile._json;
+    (accessToken, refreshToken, profile, done) => {
+        // try {
+        //     const googleAccount = profile._json;
 
-            const account = await prisma.account.findUnique({
-                where: { email: googleAccount.email }
-            })
-            console.log(account);
+        //     const account = await prisma.account.findUnique({
+        //         where: { email: googleAccount.email }
+        //     })
+        //     console.log(account);
 
-            // account not registered yet: first login -> create new account
-            if (!account) {
-                console.log('create new account')
-                const googlePicture = (await axios.get(googleAccount.picture, { responseType: 'arraybuffer' })).data;
-                const pictureBuffer = Buffer.from(googlePicture, 'base64');
-                const { pictureUrl, pictureId } = await dropbox.uploadImage(pictureBuffer);
-                const newAccount = {
-                    email: googleAccount.email,
-                    username: googleAccount.email,
-                    gender: 'not_defined',
-                    firstName: googleAccount.given_name,
-                    lastName: googleAccount.family_name,
-                    hashedPassword: '',
-                    pictureUrl,
-                    pictureId,
-                    phoneNumber: '',
-                }
+        //     // account not registered yet: first login -> create new account
+        //     if (!account) {
+        //         console.log('create new account')
+        //         const googlePicture = (await axios.get(googleAccount.picture, { responseType: 'arraybuffer' })).data;
+        //         const pictureBuffer = Buffer.from(googlePicture, 'base64');
+        //         const { pictureUrl, pictureId } = await dropbox.uploadImage(pictureBuffer);
+        //         const newAccount = {
+        //             email: googleAccount.email,
+        //             username: googleAccount.email,
+        //             gender: 'not_defined',
+        //             firstName: googleAccount.given_name,
+        //             lastName: googleAccount.family_name,
+        //             hashedPassword: '',
+        //             pictureUrl,
+        //             pictureId,
+        //             phoneNumber: '',
+        //         }
 
-                newAccount.chatAccountId = await weavy.createUser(newAccount)
+        //         newAccount.chatAccountId = await weavy.createUser(newAccount)
 
-                const newAccountData = await prisma.account.create({
-                    data: newAccount
-                })
+        //         const newAccountData = await prisma.account.create({
+        //             data: newAccount
+        //         })
 
-                console.log(newAccountData)
-            }
+        //         console.log(newAccountData)
+        //     }
 
-            done(null, profile);
-        } catch (error) {
-            console.log(error);
-            done(error);
-        }
+        done(null, profile);
+        // } catch (error) {
+        //     console.log(error);
+        //     done(error);
+        // }
     }
 ));
 
