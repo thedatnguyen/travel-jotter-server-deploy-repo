33c0cae78961a87fdff1e44b583a16b89b81a98f
Thế Đạt Nguyễn <dnguyen@datoday.local>
2023-11-28 21:36:29 +0700

    test

--- middlewares/googleAuthenticate.js
+++ middlewares/googleAuthenticate.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 
 module.exports.getProfile = passport.authenticate('google', {
-    scope: ['profile', 'email']
+    scope: ['profile', 'email'],
+    failureRedirect: '/auth/google',
+    failureMessage: 'Signing sucess, redirecting ...'
 });
 
 module.exports.getSession = passport.authenticate('google', {
